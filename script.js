2c99f27c43e389afa672821201bfeadf9bf7988d
Joe Mafrici <hello@joemafrici.com>
2023-06-04 20:18:34 -0700

    moving to desktop. added username and password forms. working on parsing POST request

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var form = document.getElementById("messageForm");
-    form.addEventListener("submit", function(event) {
+    var loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        var username = document.getElementById("username").value;
+        console.log("username: " + username);
+        var password = document.getElementById("password").value;
+        console.log("password: " + password);
+        fetch("http://localhost:8080/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "username=" + encodeURIComponent(username) + " password=" + encodeURIComponent(password)
+        })
+            .then(function (response) {
+                if (response.ok) {
+                    console.log("Username and password sent successfully");
+                    return response.text();
+                } else {
+                    console.error("Error:", response.statusText);
+                    throw new Error("Request failed");
+                }
+            })
+            .then(function (responseText) {
+                console.log("Server response:", responseText);
+            })
+            .catch(function (error) {
+                console.error("Error:", error);
+            })
+    })
+    form.addEventListener("submit", function (event) {
         event.preventDefault(); // Prevent default form submission
 
-    var messageInput = document.getElementById("message");
-    var message = messageInput.value;
-    console.log(message);
-    // Send the message to the C server
-    // what if I set this path to the path to the WSL server?
-    fetch("http://localhost:8080/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: "message=" + encodeURIComponent(message)
-    })
-    .then(function(response) {
-        // Handle the response from the C server
-        if (response.ok) {
-            console.log("Message sent successfully");
-            return response.text(); // Extract response body as text
-            // Do something on success
-        } else {
-            console.error("Error:", response.statusText);
-            throw new Error("Request failed");
-        }
-    })
-    .then(function(responseText) {
-        console.log("Server response:", responseText);
-        // Do something with the response
-    })
-    .catch(function(error) {
-        console.error("Error:", error);
-        // Handle any errors that occurred during the request
+        var messageInput = document.getElementById("message");
+        var message = messageInput.value;
+        console.log(message);
+        // Send the message to the C server
+        // what if I set this path to the path to the WSL server?
+        fetch("http://localhost:8080/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "message=" + encodeURIComponent(message)
+        })
+            .then(function (response) {
+                // Handle the response from the C server
+                if (response.ok) {
+                    console.log("Message sent successfully");
+                    return response.text(); // Extract response body as text
+                    // Do something on success
+                } else {
+                    console.error("Error:", response.statusText);
+                    throw new Error("Request failed");
+                }
+            })
+            .then(function (responseText) {
+                console.log("Server response:", responseText);
+                // Do something with the response
+            })
+            .catch(function (error) {
+                console.error("Error:", error);
+                // Handle any errors that occurred during the request
+            });
     });
-});
 });
--- server.c
+++ server.c
@@ -14,21 +14,57 @@
 //***********************************************
 #define PORT_NUMBER 8080
 #define BACKLOG 5
-#define handle_error(msg) \
-    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+#define handle_error(msg)   \
+    do                      \
+    {                       \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
+
+typedef struct User
+{
+    char username[1024];
+    char password[1024];
+} User;
+
+typedef enum Method
+{
+    POST,
+    UNSUPPORTED
+} Method;
+typedef struct Header
+{
+    char *name;
+    char *value;
+    struct Header *next;
+} Header;
+typedef struct StartLine
+{
+    enum Method method;
+    char *target;
+    char *version;
+} StartLine;
+typedef struct Request
+{
+    struct StartLine startline;
+    struct Header *headers;
+    char *body;
+} Request;
 //***********************************************
 void setup();
+Request *parse_request(const char *raw);
 //***********************************************
 int main()
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) handle_error("socket");
+    if (sockfd == -1)
+        handle_error("socket");
 
     // set socket options
     int opt = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1)
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
         handle_error("setsockopt");
-    
+
     struct sockaddr_in my_addr, client_addr;
     socklen_t client_addr_size;
     memset(&my_addr, 0, sizeof(my_addr));
@@ -36,37 +72,80 @@ int main()
 
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(PORT_NUMBER);
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY); //TODO: going to need a static
-                                                 // IP at some point
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY); // TODO: going to need a static
+                                                 //  IP at some point
     if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1)
         handle_error("bind");
 
-    if (listen(sockfd, BACKLOG) == -1) handle_error("listen");
+    if (listen(sockfd, BACKLOG) == -1)
+        handle_error("listen");
 
     printf("Server listening at http://localhost:%d\n", PORT_NUMBER);
     client_addr_size = sizeof(client_addr);
     int clientfd = accept(sockfd, (struct sockaddr *)&client_addr,
-            &client_addr_size);
-    if (clientfd == -1) handle_error("accept");
+                          &client_addr_size);
+    if (clientfd == -1)
+        handle_error("accept");
 
     char recv_msg[1024];
-    size_t num_bytes = read(clientfd, (void*)recv_msg, sizeof(recv_msg) -1 );
-    if ( num_bytes == -1) handle_error("read");
+    size_t num_bytes = read(clientfd, (void *)recv_msg, sizeof(recv_msg) - 1);
+    if (num_bytes == -1)
+        handle_error("read");
     recv_msg[num_bytes] = '\0';
-    printf("%s\n", recv_msg);
-    
+    parse_request(recv_msg);
+
     char response[] = "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/plain\r\n"
-                        "Content-Length: 13\r\n"
-                        "Access-Control-Allow-Origin: *\r\n"
-                        "\r\n"
-                        "Hello, client!";
-    printf("Sending response:\t%s\n", response);
-        if (write(clientfd, (void*)response, sizeof(response) -1 ) == -1)
-            handle_error("write");
+                      "Content-Type: text/plain\r\n"
+                      "Content-Length: 13\r\n"
+                      "Access-Control-Allow-Origin: *\r\n"
+                      "\r\n"
+                      "Hello, client!";
+    // printf("Sending response:\t%s\n", response);
+    if (write(clientfd, (void *)response, sizeof(response) - 1) == -1)
+        handle_error("write");
 
     close(clientfd);
 
     close(sockfd);
     return EXIT_SUCCESS;
+}
+
+Request *parse_request(const char *raw)
+{
+    Request *request = NULL;
+    request = (Request *)malloc(sizeof(Request));
+    if (!request)
+        handle_error("malloc");
+
+    memset(request, 0, sizeof(Request));
+    char *tokenize = (char *)malloc(strlen(raw));
+    strcpy(tokenize, raw);
+
+    char *token = strtok(tokenize, " ");
+    if (strcmp(token, "POST") == 0)
+        request->startline.method = POST;
+    else
+    {
+        request->startline.method = UNSUPPORTED;
+        return request;
+    }
+
+    token = strtok(NULL, " ");
+    request->startline.target = (char *)malloc(strlen(token));
+    strcpy(request->startline.target, token);
+
+    token = strtok(NULL, "\n");
+    request->startline.version = (char *)malloc(strlen(token));
+    strcpy(request->startline.version, token);
+
+    request->headers = (Header *)malloc(sizeof(Header));
+    token = strtok(NULL, ":");
+    request->headers->name = (char *)malloc(strlen(token));
+    strcpy(request->headers->name, token);
+
+    token = strtok(NULL, "\n");
+    request->headers->value = (char *)malloc(strlen(token));
+    strcpy(request->headers->value, token);
+    printf("name: %s\nvalue: %s\n", request->headers->name, request->headers->value);
+    return request;
 }
